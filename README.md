920f1ed1cf628911ff815daa91b0ee3485e25ff2
Tyler <209157553+thall34@users.noreply.github.com>
2025-11-26 09:40:36 -0500

    Finalize JS logic and style CSS
    Rewrite JS logic to play game in HTML/DOM
    Add CSS styling to look like vintage game
    Tweaks to reduce redundant code

--- README.md
+++ README.md
@@ -18,4 +18,16 @@ Created a new playRound function to clean up the playGame function
 Encapsulated functions into 3 total factories, one that creates players, one that creates the board
 and the final one that runs through the gameplay
 
-Still need to connect Javascript to HTML/CSS
+Nov 16, 2025
+
+Connected javascript to HTML
+Added functions to add Javascript elements to HTML to play game
+
+Nov 25, 2025
+
+Spent several hours today and on a previous day rewriting javascript to better flow with HTML
+Finally in a good spot and plays a functional game in the HTML, I have a much firmer grasp on factory functions now
+
+Nov 26, 2025
+Added reset button functionality and reduced redundant code in JS
+Styled with CSS to look like a retro videogame
--- script.js
+++ script.js
@@ -1,35 +1,70 @@
-function createPlayer (name, marker) {
-    return {
-        name: name,
-        marker: marker
-    }
-}
+function playerFactory() {
+    const players = [];
 
-function drawBoard() {
-    const grid = [];
-    for (let i = 0; i < 3; i++) {
-        grid[i] = [];
-        for (let j = 0; j < 3; j++) {
-            grid[i][j] = `(${i},${j})`
+    function createPlayer(name, marker) {
+        const player = {
+            name: name,
+            marker: marker,
         };
+
+        players.push(player);
     };
-    return {grid};
+
+    return {
+        players: players,
+        createPlayer: createPlayer
+    };
 };
 
-function gamePlay () {
-    const player1 = createPlayer("Tyler", "X");
-    const player2 = createPlayer("Amber", "O");
-    let currentPlayer = player2;
-    let roundCount = 0
-    let winCondition = false;
-    const gameBoard = drawBoard();
-    const grid = gameBoard.grid;
+function gameBoardFactory() {
+    const container = document.getElementById("container");
 
-    function switchPlayer(current) {
-        if (current === player1) {
-            return current = player2;
-        } else if (current === player2) {
-            return current = player1;
+    const grid = [
+        ["0", "0", "0"],
+        ["0", "0", "0"],
+        ["0", "0", "0"]
+    ];
+
+    function printGrid() {
+        container.innerHTML = "";
+        for (let row = 0; row < grid.length; row++) {
+            const rowDiv = document.createElement("div");
+            rowDiv.classList.add("row");
+            for (let col = 0; col < grid[row].length; col++) {
+                const cell = document.createElement("div");
+                cell.classList.add("cell");
+                cell.dataset.row = row;
+                cell.dataset.col = col;
+                cell.textContent = "";
+
+                rowDiv.appendChild(cell);
+            };
+            container.appendChild(rowDiv);
+        };
+    };
+
+    return {
+        grid: grid,
+        printGrid: printGrid
+    }
+};
+
+function gameFlow(playerX, playerO) {
+    const getPlayer = playerFactory();
+    const gameBoard = gameBoardFactory();
+    const grid = gameBoard.grid;
+    const player1 = getPlayer.createPlayer(playerX, "X", "1");
+    const player2 = getPlayer.createPlayer(playerO, "O", "2");
+
+    let activePlayer = getPlayer.players[0];
+    let winCondition = false;
+    let roundCount = 0;
+
+    function playerSwitch() {
+        if (activePlayer === getPlayer.players[0]) {
+            activePlayer = getPlayer.players[1];
+        } else if (activePlayer === getPlayer.players[1]) {
+            activePlayer = getPlayer.players[0];
         };
     };
 
@@ -56,43 +91,84 @@ function gamePlay () {
         };
     };
 
-    function gameRound(player) {
+    function fullGame() {
+        const cells = document.querySelectorAll("div.cell");
+        const display = document.getElementById("display");
 
-        function addMarker(player, in1, in2) {
-            if (grid[in1][in2] === "X" || grid[in1][in2] === "O") {
-                console.log("Pick a different spot")
-                cellFull = true;
-            } else {
-                grid[in1][in2] = player.marker;
-                cellFull = false;
-            }
-        };
+        display.textContent = `${activePlayer.name} please pick a cell`
 
-        let cellFull = true;
+        cells.forEach(cell => {
+            cell.addEventListener("click", (event) => {
+                const clickedCell = event.target;
+                const row = parseInt(clickedCell.dataset.row);
+                const col = parseInt(clickedCell.dataset.col);
 
-        console.log(`${player.name} is up`)
+                if (winCondition === false && roundCount < 9) {
+                    if (grid[row][col] !== "0") {
+                        alert("already filled");
+                    } else {
+                        clickedCell.textContent = activePlayer.marker;
+                        grid[row][col] = activePlayer.marker;
 
-        while (cellFull === true) {
-            let X = prompt("Enter a horizontal grid point");
-            let Y = prompt("Enter a vertical grid point");
-            addMarker(player, X, Y);
-        };
-        
-        roundCount += 1
+                        checkWin(activePlayer);
+                        roundCount += 1;
+
+                        if (winCondition === true) {
+                            display.textContent = `${activePlayer.name} is the Winner!`;
+                        } else if (winCondition === false && roundCount === 9) {
+                            display.textContent = "Tie Game!";
+                        } else {
+                            playerSwitch(activePlayer);
+                            display.textContent = `${activePlayer.name} please pick a cell`;
+                        };
+                    };
+                };
+            });
+        });
     };
 
-    while (winCondition === false && roundCount < 9) {
-        currentPlayer = switchPlayer(currentPlayer)
-        gameRound(currentPlayer);
-        console.log(gameBoard)
-        checkWin(currentPlayer);
-    };
-
-    if (winCondition === true) {
-        console.log(`${currentPlayer.name} is the Winner!`)
-    } else {
-        console.log("Tie Game!")
-    };
+    return {
+        fullGame: fullGame,
+    }
 };
 
-gamePlay();
+function displayGame() {
+    const button = document.getElementById("button");
+    const player1Input = document.getElementById("player1");
+    const player2Input = document.getElementById("player2");
+    const display = document.getElementById("display");
+    const board = gameBoardFactory();
+    const players = playerFactory();
+
+
+    board.printGrid();
+
+    button.addEventListener("click", () => {
+        if (button.classList.contains("start")) {
+            const game = gameFlow(player1Input.value, player2Input.value);
+            game.fullGame();
+            button.classList.remove("start");
+            button.classList.add("reset");
+            button.textContent = "Reset";
+        } else if (button.classList.contains("reset")) {
+            const game = gameFlow();
+            board.grid = [
+                ["0", "0", "0"],
+                ["0", "0", "0"],
+                ["0", "0", "0"]
+            ];
+            board.printGrid();
+            players.players = [];
+            game.winCondition = false;
+            game.roundCount = 0;
+            button.classList.remove("reset");
+            button.classList.add("start");
+            button.textContent = "Start";
+            player1Input.value = "";
+            player2Input.value = "";
+            display.textContent = `Please input your names below`;
+        };
+    });
+};
+
+displayGame();
